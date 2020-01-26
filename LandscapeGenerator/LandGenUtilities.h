ae2b64d0e574b4cd7d85da7df3af40287e64e9c9
selinaZitrone <selina.baldauf@gmx.de>
2020-01-26 23:21:47 +0100

    refactoring data input structure

--- LandscapeGenerator/LandGenUtilities.h
+++ LandscapeGenerator/LandGenUtilities.h
@@ -1,8 +1,92 @@
 #pragma once
+
 using namespace std;
 #include <string>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+#include "InputOutput.h"
+#include "RandomLandscape.h"
+#include "LandGenDataStructures.h"
 
+#include <filesystem>
+namespace fs = std::filesystem;
+
+string askForPath() {
+	char path[120];
+
+	cout << "Please insert the path of the folder containing the data in json format: " << endl;
+	cin.getline(path, sizeof(path));
+	cout << " " << endl;
+
+	try {
+		for (const auto & entry : fs::directory_iterator(path)) {
+
+			if (entry.path().extension() == ".json") {
+				string jsonPath(path);
+				if (jsonPath != "") {
+					jsonPath += "\\";
+				}
+				return jsonPath;
+			}
+		}
+		cout << "no json file found, please try with another path" << endl;
+		return askForPath();
+	}
+	catch (const runtime_error& e) {
+		cout << " " << endl;
+		cerr << e.what() << endl << endl;
+		cout << " " << endl;
+		return askForPath();
+	}
+}
+
+nlohmann::json askForJsonName(string jsonPath) {
+	cout << "Found following files:" << endl;
+	cout << " " << endl;
+	for (const auto & entry : fs::directory_iterator(jsonPath)) {
+
+		if (entry.path().extension() == ".json") {
+			std::cout << entry.path().filename() << std::endl;
+		}
+	}
+	cout << endl << "Please choose the name of the file you would like to open (without extension): " << endl;
+
+	char nameOfJson[120];
+
+	cin.getline(nameOfJson, sizeof(nameOfJson));
+
+	nlohmann::json landscapeElements;
+
+	string b(nameOfJson);
+	string toLoad = jsonPath + b + ".json";
+
+	try {
+		landscapeElements = InputOutput::readLandscapeUserInput(toLoad);
+		return landscapeElements;
+	}
+	catch (const runtime_error& e) {
+		cerr << e.what() << endl << endl;
+		return askForJsonName(jsonPath);
+	}
+}
+
+unsigned int askForAreaSize() {
+	unsigned int areaSize;
+	cout << endl << "Please define the dimension of the surface (one integer to define the size of one side of the square area)" << endl;
+	cin >> areaSize;
+	if (cin.good()) {
+		return areaSize;
+	}
+	else {
+		cout << endl;
+		cout << "Invalid entry, it has to be a positive integer." << endl << endl;
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		return askForAreaSize();
+	}
+}
 
 string printTable(vector<double> toPrint, int sideSize) {
 	int side = sideSize;
--- LandscapeGenerator/LandscapeGenerator.cpp
+++ LandscapeGenerator/LandscapeGenerator.cpp
@@ -9,63 +9,16 @@
 
 using namespace std;
 
-#include <filesystem>
-namespace fs = std::filesystem;
-
-
-
 int main()
 {
-	char path[120];
-	cout << "Please insert the path of the folder containing the data in json format: " << endl;
-	cin.getline(path, sizeof(path));
-	cout << " " << endl;
+	string jsonPath = askForPath();
 
-	try {
-		cout << "Found following files:" << endl;
-		cout << " " << endl;
-		for (const auto & entry : fs::directory_iterator(path)) {
-			
-			if (entry.path().extension() == ".json") {
-				std::cout << entry.path().filename() << std::endl;
-			}
-		}
-	}
-	catch (const runtime_error& e) {
-		cout << " " << endl;
-		cerr << e.what() << endl;;
-		cout << " " << endl;
-		return -1;
-	}
-
-	cout << "Please choose the name of the file you would like to open (without extension): " << endl;
-	char nameOfJson[100];
-	cin.getline(nameOfJson, sizeof(nameOfJson));
-
-
-
-    // read a json file
-    nlohmann::json landscapeElements;
-
-	string a(path);
-	string b(nameOfJson);
-	string toLoad = a + "\\" + b + ".json";
-
-    try {
-        landscapeElements = InputOutput::readLandscapeUserInput(toLoad);
-    }
-    catch (const runtime_error& e) {
-        cerr << e.what() << endl;;
-        return -1;
-    }
-
-	cout << endl << "Please define the dimension of the surface (one integer to define the size of one side of the square area)" << endl;
-   
-	int areaSize;
-	cin >> areaSize;
-	cout << endl;
-
+	nlohmann::json landscapeElements = askForJsonName(jsonPath);
+	
+	unsigned int areaSize = askForAreaSize();
+	
 	RandomLandscape firstRandom(areaSize);
+
 	firstRandom.generateLandscape(landscapeElements);
 
 	map<string, mapStruct> soilMaps = firstRandom.getAllSoilMaps();
@@ -75,22 +28,4 @@ int main()
 	printMaps(soilMaps, "SOIL MAPS");
 	printMaps(crustMaps, "CRUST MAPS");
 	printMaps(vascularMaps, "VASCULAR MAPS");
-
-
-	//cout << printTable(firstRandom.getSoilMap("sS").valuesVector, firstRandom.getSize());
-	//cout << " - - - " << endl;
-	//cout << printTable(firstRandom.getSoilMap("lS").valuesVector, firstRandom.getSize());
-	//cout << " - - - " << endl;
-	//cout << printTable(firstRandom.getSoilMap("kA").valuesVector, firstRandom.getSize());
-	//cout << " - - - " << endl;
-	//cout << "my size: " << firstRandom.getSize() << endl;
-	//cout << " - - - " << endl;
-	//cout << printTable(firstRandom.getCrustMap("moss").valuesVector, firstRandom.getSize()) << endl;
-	//cout << " - - - " << endl;
-	//cout << printTable(firstRandom.getCrustMap("lichen").valuesVector, firstRandom.getSize()) << endl;
-	//cout << " - - - " << endl;
-	//cout << printTable(firstRandom.getVascularMap("shrub").valuesVector, firstRandom.getSize()) << endl;
-	//cout << " - - - " << endl;
-	//cout << printTable(firstRandom.getVascularMap("alessio").valuesVector, firstRandom.getSize()) << endl;
-	//cout << " - - - " << endl;
 }
